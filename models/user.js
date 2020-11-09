f33595a8eeb27c579c51f4c03f31a1d00a5590f0
Tasikul Islam <iamtasikul@gmail.com>
2020-11-09 16:08:44 +0530

    Virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -36,3 +36,31 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//Virtual Field
+userSchema
+  .virtual("password") //sending password from client side
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidvi1(); //gives random strings
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
